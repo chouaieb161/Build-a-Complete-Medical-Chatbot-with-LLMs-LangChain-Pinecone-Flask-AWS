75497b3a2f98e6bd783899de79f135e35f0b60f1
chouaieb161 <dridichouaieb07@gmail.com>
2025-07-27 08:20:32 +0100

    folder structure

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src
+mkdir -p research
+
+#file:
+touch src/__init__.py
+touch src/helper
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.py
+touch requirements.txt
+
+
+echo "directories and files created successfully."
